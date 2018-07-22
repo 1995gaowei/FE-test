21f9c718e9b78c240c847a9881ec6e12bb0a4d50
1995gaowei <15850535810@163.com>
2018-07-22 22:53:28 +0800

    add notes

--- test1.js
+++ test1.js
@@ -1,34 +1,38 @@
-var array = [2, 5, 1, 1, 1, 1, 4, 1, 7, 3, 7];
-var array1 = [10, 2, 11, 13, 1, 1, 1, 1, 1];
-
-function getHashMap(arr) {
-  let hash = {};
-  arr.reduce((acc, val) => {
-    hash[acc] = val;
-    return acc + val;
-  });
-  return hash;
-}
-
-function isQuarter(hashMap) {
-  let keys = Object.keys(hashMap);
-  for (let index = 0; index < keys.length; index++) {
-    let quarter = parseInt(keys[index]);
-    // console.log("round"+index +"  quarter:" +  parseInt(keys[index]));
-    let half = quarter * 2 + hashMap[quarter];
-    // console.log("    half:"+half);
-    if (!half) continue;
-    let threeQuarters = half + quarter + hashMap[half];
-    // console.log("    threeQuarters:"+threeQuarters);
-    if (!threeQuarters) continue;
-    let full = threeQuarters + quarter + hashMap[threeQuarters];
-    // console.log("    full:"+full);
-    if (!full) continue;
-    console.log(keys[index], half, threeQuarters, full);
-    return true;
+function t(arr, parentId) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].parent == parentId) {
+      let target = arr[i];
+      arr.splice(i, 1);
+      let child = t(arr, target.id);
+      if (child == null)
+        return {
+          id: target.id,
+          parent: target.parent
+        };
+      else
+        return {
+          id: target.id,
+          parent: target.parent,
+          child
+        };
+    }
   }
-  return false;
+
+  return null;
 }
 
-console.log(isQuarter(getHashMap(array)));
-console.log(isQuarter(getHashMap(array1)));
+var obj1 = [
+  { id: 1, parent: null },
+  { id: 2, parent: 1 },
+  { id: 3, parent: 2 }
+];
+
+var obj2 = {
+  obj: t(obj1, null)
+};
+console.log(obj2);
+
+function a(...nums) {
+  console.log(nums.join(" "));
+}
+a("1", "2", "3");
